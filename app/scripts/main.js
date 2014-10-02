461301533161ebec1ecdb257a4ec15ee11254c2f
Jonathan Dickerson <jongdickerson@gmail.com>
2014-10-02 16:04:14 -0400

    dried out js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,168 +1,26 @@
 
 var playerturn = 0;
 
-$('.one').click(function() {
-    if($( ".one" ).hasClass("clicked")) {
+$('.one, .two, .three, .four, .five, .six, .seven, .eight, .nine').click(function() {
+    console.log("clicked");
+    if($(this).hasClass("clicked")) {
         alert("choose another");
     } else if (playerturn % 2 != 0) {
-        $('.one').append('<p>X</p>');
-        $('.one').addClass('x');
+        $(this).append('<p>X</p>');
+        $(this).addClass('x');
         playerturn += 1;
             console.log(playerturn);
-        $('.one').addClass('clicked');
+        $(this).addClass('clicked');
     } else { 
-        $('.one').append('<p>O</p>');
-        $('.one').addClass('o');
+        $(this).append('<p>O</p>');
+        $(this).addClass('o');
         playerturn += 1;
             console.log(playerturn);
-        $('.one').addClass('clicked');
+        $(this).addClass('clicked');
     };
+
 });
 
-$('.two').click(function() {
-    if($( ".two" ).hasClass("clicked")) {
-        alert("choose another");
-    } else if (playerturn % 2 != 0) {
-        $('.two').append('<p>X</p>');
-        $('.two').addClass('x');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.two').addClass('clicked');
-    } else { 
-        $('.two').append('<p>O</p>');
-        $('.two').addClass('o');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.two').addClass('clicked');
-    };
-});
-
-$('.three').click(function() {
-    if($( ".three" ).hasClass("clicked")) {
-        alert("choose another");
-    } else if (playerturn % 2 != 0) {
-        $('.three').append('<p>X</p>');
-        $('.three').addClass('x');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.three').addClass('clicked');
-    } else { 
-        $('.three').append('<p>O</p>');
-        $('.three').addClass('o');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.three').addClass('clicked');
-    };
-});
-
-$('.four').click(function() {
-    if($( ".four" ).hasClass("clicked")) {
-        alert("choose another");
-    } else if (playerturn % 2 != 0) {
-        $('.four').append('<p>X</p>');
-        $('.four').addClass('x');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.four').addClass('clicked');
-    } else { 
-        $('.four').append('<p>O</p>');
-        $('.four').addClass('o');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.four').addClass('clicked');
-    };
-});
-
-$('.five').click(function() {
-    if($( ".five" ).hasClass("clicked")) {
-        alert("choose another");
-    } else if (playerturn % 2 != 0) {
-        $('.five').append('<p>X</p>');
-        $('.five').addClass('x');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.five').addClass('clicked');
-    } else { 
-        $('.five').append('<p>O</p>');
-        $('.five').addClass('o');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.five').addClass('clicked');
-    };
-});
-
-$('.six').click(function() {
-    if($( ".six" ).hasClass("clicked")) {
-        alert("choose another");
-    } else if (playerturn % 2 != 0) {
-        $('.six').append('<p>X</p>');
-        $('.six').addClass('x');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.six').addClass('clicked');
-    } else { 
-        $('.six').append('<p>O</p>');
-        $('.six').addClass('o');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.six').addClass('clicked');
-    };
-});
-
-
-$('.seven').click(function() {
-    if($( ".seven" ).hasClass("clicked")) {
-        alert("choose another");
-    } else if (playerturn % 2 != 0) {
-        $('.seven').append('<p>X</p>');
-        $('.seven').addClass('x');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.seven').addClass('clicked');
-    } else { 
-        $('.seven').append('<p>O</p>');
-        $('.seven').addClass('o');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.seven').addClass('clicked');
-    };
-});
-
-$('.eight').click(function() {
-    if($( ".eight" ).hasClass("clicked")) {
-        alert("choose another");
-    } else if (playerturn % 2 != 0) {
-        $('.eight').append('<p>X</p>');
-        $('.eight').addClass('x');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.eight').addClass('clicked');
-    } else { 
-        $('.eight').append('<p>O</p>');
-        $('.eight').addClass('o');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.eight').addClass('clicked');
-    };
-});
-
-$('.nine').click(function() {
-    if($( ".nine" ).hasClass("clicked")) {
-        alert("choose another");
-    } else if (playerturn % 2 != 0) {
-        $('.nine').append('<p>X</p>');
-        $('.nine').addClass('x');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.nine').addClass('clicked');
-    } else { 
-        $('.nine').append('<p>O</p>');
-        $('.nine').addClass('o');
-        playerturn += 1;
-            console.log(playerturn);
-        $('.nine').addClass('clicked');
-    };
-});
 
 $('.reset').click(function() {
     $('.one').removeClass('clicked');
